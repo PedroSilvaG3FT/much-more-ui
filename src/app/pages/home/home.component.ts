c0a503fbf1ba0b8c9118e080b059e17dca98b92a
Pedro Silva <pedrohenriqueuh2010@hotmail.com>
2020-10-01 21:22:58 -0300

    Layout base home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  public timer = [
+    { value: 120, label: 'Days' },
+    { value: 21, label: 'Hours' },
+    { value: 17, label: 'Minutes' },
+    { value: 25, label: 'Seconds' },
+  ];
   constructor() { }
 
   ngOnInit(): void {
